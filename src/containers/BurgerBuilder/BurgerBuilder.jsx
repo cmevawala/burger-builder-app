10706246c48c31345f56d0c5d7773c6fb3caf408
cmevawala <chintu.m18@gmail.com>
2019-02-10 12:27:42 +0530

    Starting Burger Builder Container implementation

---
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxilary';
+
+
+class BurgerBuilder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    render() { 
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>  
+        );
+    }
+}
+ 
+export default BurgerBuilder;
